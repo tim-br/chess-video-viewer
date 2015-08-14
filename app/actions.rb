43cf00d3c4655ac8a9c87d3e7128c8f362ebcaf4
Timothy <t.williams.im@gmail.com>
2015-08-13 22:23:52 -0400

    added main view, with menu

--- app/actions.rb
+++ app/actions.rb
@@ -9,6 +9,7 @@ end
 def new_video(title, url)
   Video.create(title: title, url: url)
 end
+
 # Homepage (Root path)
 get '/' do
   erb :index
@@ -16,6 +17,11 @@ end
 
 get '/yolo' do
   "yolo"
+end
+
+get '/mainview' do
+  @videos = get_all_videos
+  erb :main_view
 end
 
 get '/videos' do
@@ -27,6 +33,10 @@ end
 get '/videos/:id/url' do
   content_type :json
   get_video_url(params[:id])
+end
+
+get '/videos/:id/view' do
+  "yolo"
 end
 
 post '/videos' do
