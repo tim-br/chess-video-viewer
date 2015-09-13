b74a12ec929a851850473f3f5f11e223f1ffbbf0
Timothy <t.williams.im@gmail.com>
2015-09-13 13:28:42 -0400

    changed a migration

--- db/migrate/20150913165416_change_admin_password_and_add_admin.rb
+++ db/migrate/20150913165416_change_admin_password_and_add_admin.rb
@@ -1,6 +1,7 @@
 class ChangeAdminPasswordAndAddAdmin < ActiveRecord::Migration
   def change
  	 User.find_by(full_name: 'Jacob Stein').update_attributes(password: 'jacobadmin', password_confirmation: 'jacobadmin')
+ 	 User.find_by(full_name: 'Jacob Stein').save
  	 User.create full_name: "Artiom", email: "samsonkin@list.ru", password: "artiomadmin", password_confirmation: "artiomadmin", is_admin: "true"
   end
 end
