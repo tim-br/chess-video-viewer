7c70edc68b59e6d07cb24cd2f6eab5a15679c086
tmtwd <t.williams.im@gmail.com>
2016-08-30 15:11:32 -0400

    rm pdf

--- app/api.rb
+++ app/api.rb
@@ -1,16 +1,8 @@
 def all_videos_for_semester(level, semester)
-  if level == 1 && semester == 1
-    Video.where("is_beginner = ? AND semester_number = ?", true, 1).order("week_number")
-  elsif level == 2 && semester ==1
-    Video.where("is_advanced = ? AND semester_number = ?", true, 1).order("week_number")
-  elsif level == 1 && semester == 2
-    Video.where("is_beginner = ? AND semester_number = ?", true, 2).order("week_number")
-  elsif level == 2 && semester == 2
-    Video.where("is_advanced = ? AND semester_number = ?", true, 2).order("week_number")
-  elsif level == 2 && semester == 3
-    Video.where("is_advanced = ? AND semester_number = ?", true, 3).order("week_number")
-  elsif level == 1 && semester == 3
-    Video.where("is_beginner = ? AND semester_number = ?", true, 3).order("week_number")
+  if level == 1
+    Video.where({is_beginner: true, semester_number: semester})
+  elsif level == 2
+    Video.where({is_advanced: true, semester_number: semester})
   end
 end
 
@@ -18,3 +10,18 @@ end
 #   Video.where("is_beginner = ? AND semester_number = ?", true, semester).order("week_number")
 # elsif level == 2
 #   Video.where("is_advanced = ? AND semester_number = ?", true, semester).order("week_number")
+
+
+# if level == 1 && semester == 1
+#   Video.where("is_beginner = ? AND semester_number = ?", true, 1).order("week_number")
+# elsif level == 2 && semester ==1
+#   Video.where("is_advanced = ? AND semester_number = ?", true, 1).order("week_number")
+# elsif level == 1 && semester == 2
+#   Video.where("is_beginner = ? AND semester_number = ?", true, 2).order("week_number")
+# elsif level == 2 && semester == 2
+#   Video.where("is_advanced = ? AND semester_number = ?", true, 2).order("week_number")
+# elsif level == 2 && semester == 3
+#   Video.where("is_advanced = ? AND semester_number = ?", true, 3).order("week_number")
+# elsif level == 1 && semester == 3
+#   Video.where("is_beginner = ? AND semester_number = ?", true, 3).order("week_number")
+# end
